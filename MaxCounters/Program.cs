b8fe3a96b0415d9c58c92bc6a4f191194eaeedef
Welly Tambunan <if05041@gmail.com>
2014-03-19 19:12:49 +0700

    add 2 solution

---
+++ MaxCounters/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MaxCounters
+{
+    using System;
+    // you can also use other imports, for example:
+    // using System.Collections.Generic;
+    class Solution
+    {
+        public int[] solution(int N, int[] A)
+        {
+            var counters = new int[N];
+            int max = 0;
+            int resetLimit = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] <= N)
+                {
+                    //counters[A[i] - 1]++;
+
+                    if (counters[A[i] - 1] < resetLimit)
+                    {
+                        counters[A[i] - 1] = resetLimit + 1;
+                    }
+                    else
+                    {
+                        counters[A[i] - 1]++;
+                    }
+
+                    max = Math.Max(max, counters[A[i] - 1]);
+                }
+                else
+                {
+                    resetLimit = max;
+                }
+            }
+
+            for (int i = 0; i < counters.Length; i++)
+            {
+                counters[i] = Math.Max(resetLimit, counters[i]);
+            }
+
+            return counters;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var result = new Solution().solution(5, new[] { 3, 4, 4, 6, 1, 4, 4 });
+            var result = new Solution().solution(1, new[] { 1, 1, 1 });
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ PassingCars/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingCars
+{
+    using System;
+    // you can also use other imports, for example:
+    // using System.Collections.Generic;
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int eastCar = 0;
+            int passingCar = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 0)
+                {
+                    eastCar++;
+                }
+                else
+                {
+                    passingCar += eastCar;
+                }
+            }
+
+            if (passingCar < 0 || passingCar > 1e9)
+                return -1;
+            else return passingCar;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().solution(new []{0, 1, 0, 1, 1}));
+
+        }
+    }
+}
